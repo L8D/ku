51fe3c0a905d36ae66ccedf791ffa4b56b610fc3
L8D <tenorbiel@gmail.com>
2014-11-05 17:41:07 -0600

    add istanbul flag

--- ku.js
+++ ku.js
@@ -3,9 +3,10 @@
  * @license MIT
  * @copyright (c) 2014 Tenor Biel
  */
-;(function(root, factory) {
+; /* istanbul ignore next: UMD */
+(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(factory);
+    define('ku', factory);
   } else if (typeof exports === 'object') {
     module.exports = factory();
   } else {
