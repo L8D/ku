4cc797f65d219cb8f27518df5a3f21590cceb4b5
L8D <tenorbiel@gmail.com>
2014-11-06 14:32:25 -0600

    switch to mocha instead of tape

--- test/index.js
+++ test/index.js
@@ -1,65 +1,92 @@
-var test = require('tape');
+var assert = require('assert');
 var ku = require('../ku');
 
-test('map', function(t) {
-  t.plan(2);
+describe('map', function() {
+  it('should map over a non-empty array', function() {
+    var res = ku.map(function(x) {
+      return x * 3;
+    }, [1, 2, 3]);
 
-  var res = ku.map(function(x) { return x * 3; }, [1, 2, 3]);
+    assert.deepEqual(res, [3, 6, 9]);
+  });
 
-  t.deepEqual(res, [3, 6, 9]);
+  it('should map over an empty array', function() {
+    res = ku.map(function(x) {
+      return x * 3
+    }, []);
 
-  res = ku.map(function(x) { return x * 3 }, []);
-
-  t.deepEqual(res, []);
+    assert.deepEqual(res, []);
+  });
 });
 
-test('filter', function(t) {
-  t.plan(2);
+describe('filter', function() {
+  it('should filter elements from a non-empty array', function() {
+    var res = ku.filter(function(x) {
+      return x % 2 === 0;
+    }, [1, 2, 3, 4, 5, 6]);
 
-  var res = ku.filter(function(x) { return x % 2 === 0; }, [1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(res, [2, 4, 6]);
+  });
 
-  t.deepEqual(res, [2, 4, 6]);
+  it('should filter elements from an empty', function() {
+    var res = ku.filter(function(x) {
+      return x % 2 === 0;
+    }, []);
 
-  res = ku.filter(function(x) { return x % 2 === 0; }, []);
-
-  t.deepEqual(res, []);
+    assert.deepEqual(res, []);
+  });
 });
 
-test('reduce', function(t) {
-  t.plan(2);
+describe('reduce', function() {
+  it('should aggregate a value from a non-empty array', function() {
+    var res = ku.reduce(function(x, y) {
+      return x + y;
+    }, 0, [1, 2, 3]);
 
-  var res = ku.reduce(function(x, y) { return x + y; }, 0, [1, 2, 3]);
+    assert.equal(res, 6);
+  });
 
-  t.equal(res, 6);
+  it('should return accumulator when reducing an empty array', function() {
+    var res = ku.reduce(function(x, y) {
+      return x + y;
+    }, 0, []);
 
-  res = ku.reduce(function(x, y) { return x + y; }, 0, []);
-
-  t.equal(res, 0);
+    assert.equal(res, 0);
+  });
 });
 
-test('scan', function(t) {
-  t.plan(2);
+describe('scan', function() {
+  it('should aggregate an array of values from a non-empty array', function() {
+    var res = ku.scan(function(x, y) {
+      return x + y;
+    }, 0, [1, 2, 3]);
 
-  var res = ku.scan(function(x, y) { return x + y; }, 0, [1, 2, 3]);
+    assert.deepEqual(res, [0, 1, 3, 6]);
+  });
 
-  t.deepEqual(res, [0, 1, 3, 6]);
+  it('should return accumulator when scanning an empty array', function() {
+    var res = ku.scan(function(x, y) {
+      return x + y;
+    }, 0, []);
 
-  res = ku.scan(function(x, y) { return x + y; }, 0, []);
-
-  t.deepEqual(res, [0]);
+    assert.deepEqual(res, [0]);
+  });
 });
 
-test('chain', function(t) {
-  t.plan(2);
+describe('chain', function() {
+  it('should map over a non-empty array and concatenate result', function() {
+    var res = ku.chain(function(x) {
+      return [x, x + 1];
+    }, [1, 2, 3, 4, 5]);
 
-  var res = ku.chain(
-    function(x) { return x; },
-    [[1, 2, 3], [4], [5], [6], [], [7], [8, 9], [[10, 11], 12]]
-  );
+    assert.deepEqual(res, [1, 2, 2, 3, 3, 4, 4, 5, 5, 6]);
+  });
 
-  t.deepEqual(res, [1, 2, 3, 4, 5, 6, 7, 8, 9, [10, 11], 12]);
+  it('should map over an empty array and return it', function() {
+    var res = ku.chain(function(x) {
+      return [x, x + 1];
+    }, []);
 
-  res = ku.chain(function(x) { return x; }, []);
-
-  t.deepEqual(res, []);
+    assert.deepEqual(res, []);
+  });
 });
